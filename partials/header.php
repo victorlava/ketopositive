8d97381c7ee8eaeb87c9d0bcc50f45ad92930853
sveiki@viktoraslava.lt <sveiki@viktoraslava.lt>
2017-02-04 22:27:57 +0000

    added DOM header

--- partials/header.php
+++ partials/header.php
@@ -7,18 +7,14 @@
     <meta name="contact" content="hello@victorlava.com" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui">
-    <title>Motorent</title>
+    <title>Kelioniupaieska.lt</title>
     <meta name="description" content=""/> 
-    <link rel="stylesheet" href="/assets/css/main.css" type="text/css" media="all"> 
-    <link rel="stylesheet" href="/assets/css/jRange.min.css"> 
-    <link rel="stylesheet" href="/assets/css/lightbox.min.css">
-    <link rel="stylesheet" href="/assets/css/sprites.min.css">
-    <link rel="stylesheet" href="/assets/css/bootstrap-formhelpers.min.css">  
+    <link rel="stylesheet" href="/assets/css/style.css" type="text/css" media="all"> 
+    <link rel="stylesheet" href="/assets/css/sprites.css">
     <link rel="stylesheet" href="/bower_components/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/bower_components/bootstrap-datepicker-1.6.4-dist/css/bootstrap-datepicker.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
     <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="/assets/js/script.min.js"></script>
+    <script src="/assets/js/src/script.js"></script>
 
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
