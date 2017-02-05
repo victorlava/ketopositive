cce6006847964018e3fd8fa37c4dfff003d5d05e
sveiki@viktoraslava.lt <sveiki@viktoraslava.lt>
2017-02-05 17:08:17 +0000

    added footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,72 +1,75 @@
 
 	        <section class="footer section">
 	        	<footer>
-		        	<div class="container">
-		        		<div class="footer-top row">
-			        		<div class="col-md-3">
-			        			<div class="footer-block">
-			        				<header class="header header--line header--left">
-			        					<h4 class="header-title">Apie kelioniupaieska.lt</h4>
-			        				</header>
-				        			<p>Profesionalų komanda, sukaupusi ilgametę patirtį ir padedanti įsigyti keliones į bet kurį pasaulio kraštą. Siūlome aviabilietus, traukinių, autobusų, keltų bilietus, viešbučius visame pasaulyje. Mūsų biuruose gausite išsamią, tikslią ir naujausią informaciją.</p>
-			        			</div>
-			        		</div>
+	        			<div class="footer-top row">
+				        	<div class="container">
+				        		<div class="col-md-3">
+				        			<div class="footer-block">
+				        				<header class="header header--line header--left">
+				        					<h4 class="header-title">Apie kelioniupaieska.lt</h4>
+				        				</header>
+					        			<p>Profesionalų komanda, sukaupusi ilgametę patirtį ir padedanti įsigyti keliones į bet kurį pasaulio kraštą. Siūlome aviabilietus, traukinių, autobusų, keltų bilietus, viešbučius visame pasaulyje. Mūsų biuruose gausite išsamią, tikslią ir naujausią informaciją.</p>
+				        			</div>
+				        		</div>
 
-			        		<div class="col-md-3">
-			        			<div class="footer-block">
-			        				<header class="header header--line header--left">
-			        					<h4 class="header-title">Agentūros filialai</h4>
-			        				</header>
-			        				<ul class="list list--phones">
-			        					<li><a href="call:852124474">Vilnius: 8 5 21244 74</a></li>
-			        					<li><a href="call:852124474">Vilnius: 8 5 21244 74</a></li>
-			        					<li><a href="call:852124474">Vilnius: 8 5 21244 74</a></li>
-			        					<li><a href="call:852124474">Vilnius: 8 5 21244 74</a></li>
-			        				</ul>
-			        			</div>
-			        		</div>
+				        		<div class="col-md-3">
+				        			<div class="footer-block">
+				        				<header class="header header--line header--left">
+				        					<h4 class="header-title">Agentūros filialai</h4>
+				        				</header>
+				        				<ul class="list list--phones">
+				        					<li><a href="call:852124474"><strong>Vilnius:</strong> 8 5 21244 74</a></li>
+				        					<li><a href="call:852124474"><strong>Vilnius:</strong> 8 5 21244 74</a></li>
+				        					<li><a href="call:852124474"><strong>Vilnius:</strong> 8 5 21244 74</a></li>
+				        					<li><a href="call:852124474"><strong>Vilnius:</strong> 8 5 21244 74</a></li>
+				        				</ul>
+				        			</div>
+				        		</div>
 
-			        		<div class="col-md-3">
-			        			<div class="footer-block">
-			        				<header class="header header--line header--left">
-			        					<h4 class="header-title">Naudinga informacija</h4>
-			        				</header>
-				        			<ul class="list list--links">
-			        					<li><a href="#">Madeira – amžina gėlių sala </a></li>
-			        					<li><a href="#">Ilgasis kovo savaitgalis</a></li>
-			        					<li><a href="#">Patarimai planuojantiems kelionę savarankiškai, bet norintiems </a></li>
-			        					<li><a href="#">3 vietos, kurių dar nematėte Europoje</a></li>
-			        				</ul>
-			        			</div>
-			        		</div>
+				        		<div class="col-md-3">
+				        			<div class="footer-block">
+				        				<header class="header header--line header--left">
+				        					<h4 class="header-title">Naudinga informacija</h4>
+				        				</header>
+					        			<ul class="list list--links">
+				        					<li><a href="#">Madeira – amžina gėlių sala </a></li>
+				        					<li><a href="#">Ilgasis kovo savaitgalis</a></li>
+				        					<li><a href="#">Patarimai planuojantiems kelionę savarankiškai, bet norintiems </a></li>
+				        					<li><a href="#">3 vietos, kurių dar nematėte Europoje</a></li>
+				        				</ul>
+				        			</div>
+				        		</div>
 
-			        		<div class="col-md-3">
-			        			<div class="footer-block">
-			        				<header class="header header--line header--left">
-			        					<h4 class="header-title">Kelionės</h4>
-			        				</header>
-				        			<ul class="list list--links">
-			        					<li><a href="#">Novaturo kelionių pasiūlymai</a></li>
-			        					<li><a href="#">Teztour kelionių pasiūlymai</a></li>
-			        					<li><a href="#">Karščiausi kelionių pasiūlymai</a></li>
-			        					<li><a href="#">Pažintinės kelionės lėktuvu ir autobusu</a></li>
-			        				</ul>
-			        			</div>
-			        		</div>
+				        		<div class="col-md-3">
+				        			<div class="footer-block">
+				        				<header class="header header--line header--left">
+				        					<h4 class="header-title">Kelionės</h4>
+				        				</header>
+					        			<ul class="list list--links">
+				        					<li><a href="#">Novaturo kelionių pasiūlymai</a></li>
+				        					<li><a href="#">Teztour kelionių pasiūlymai</a></li>
+				        					<li><a href="#">Karščiausi kelionių pasiūlymai</a></li>
+				        					<li><a href="#">Pažintinės kelionės lėktuvu ir autobusu</a></li>
+				        				</ul>
+				        			</div>
+				        		</div>
+				        	</div> 
 		        		</div>
-		        		<div class="footer-bottom row">
-		        			<div class="footer-copyright pull-left">
-		        				Kelioniupaieska.lt ® 2 016
-		        			</div>
-		        			<div class="footer-social pull-right">
-		        				<ul class="list list--inline">
-		        					<li><a href="#"><i class="fa"></i></a></li>
-		        					<li><a href="#"><i class="fa"></i></a></li>
-		        					<li><a href="#"><i class="fa"></i></a></li>
-		        				</ul>
+
+		        		<div class="footer-bottom">
+		        			<div class="container">
+		        				<div class="footer-copyright pull-left">
+			        				Kelioniupaieska.lt ® 2016
+			        			</div>
+			        			<div class="footer-social pull-right">
+			        				<ul class="list list--inline">
+			        					<li><a href="#"><i class="fa"></i></a></li>
+			        					<li><a href="#"><i class="fa"></i></a></li>
+			        					<li><a href="#"><i class="fa"></i></a></li>
+			        				</ul>
+			        			</div>
 		        			</div>
 		        		</div>
-		        	</div> 
 		        </footer>
 	        </section>
 
