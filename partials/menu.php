f6d8b11bfd37fccb9d65d1809345b21c900c20b7
sveiki@viktoraslava.lt <sveiki@viktoraslava.lt>
2017-02-05 00:16:08 +0000

    add navbar

--- partials/menu.php
+++ partials/menu.php
@@ -1,66 +1,62 @@
 <nav class="navbar navbar--main">
   <div class="container">
-    <div class="row">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">
-          <i class="icon-logo"></i>
-        </a>
-      </div>
+    <div class="navbar-top row">
+        <a class="navbar-brand pull-left" href="/">
+          Kelioniupaieska.lt
+        </a> 
+
+        <div class="navbar-top-right">
+          <div class="navbar-top-menu">
+            <ul class="nav nav--secondary nav--dropdown">
+              <li>
+                <a href="#">Vizos</a>
+              </li>
+              <li>
+                <a href="#">Autonuoma</a>
+              </li>
+              <li>
+                <a href="#">Draudimas</a>
+              </li>
+              <li class="dropdown">    
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Kontaktai</a>
+                <ul class="dropdown-menu dropdown-menu--sign">
+                  <!-- inside kontaktai dropdown -->
+                </ul>
+              </li>
+            </ul>
+          </div>
+
+          <div class="navbar-top-phone">
+            <!-- savaičių dienų partial -->
+            <a href="tel:+37052124474">+370 5 212 4474</a>
+          </div>
+
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+
+        </div><!-- .navbar-top-right -->
+
+    </div><!-- .navbar-top -->
+
+    <div class="navbar-bottom row">
       <div id="navbar" class="navbar-collapse collapse in hidden-xs hidden-sm" aria-expanded="true">
-        <ul class="nav navbar-nav navbar-menu">
-          <li class="active"><a href="#">Motorcycle Rentals</a></li>
-          <li><a href="#about">Motorcycle Tours</a></li>
-        </ul>
-
-        <ul class="nav navbar-nav navbar--dropdown pull-right">
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="icon icon-globe"></i> EN</a>
-            <ul class="dropdown-menu">
-              <li><a href="/">RU</a></li>
-              <li><a href="/">DE</a></li>
-              <li><a href="/">ES</a></li> 
-            </ul>
-          </li>
-          <li id="sign" class="dropdown dropdown-button">    
-            <a href="#" class="dropdown-toggle dropdown-toggle-sign button button--primary button--small" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="icon icon-helmet"></i> Sign In</a>
-            <ul class="dropdown-menu dropdown-menu--sign">
-              <h3 class="title">Sign In</h3>
-              <form role="form">
-                <div class="input-group">
-                  <span class="input-group-addon" id="basic-addon1"><i class="icon icon-email-blue input-group-icon"></i></span>
-                  <input type="text" id="email" name="email" class="form-control" placeholder="E-mail address" aria-describedby="basic-addon1">
-                </div>
-
-                <div class="input-group">  
-                  <span class="input-group-addon" id="basic-addon2"><i class="icon icon-lock-blue input-group-icon"></i></span>
-                  <input type="text" id="password" name="password" class="form-control" placeholder="Enter your password" aria-describedby="basic-addon2">
-                </div>
-
-                <p class="dropdown-menu-forgot"><a href="#">Forgot password?</a></p>
-
-                <input type="submit" class="button button--primary" value="Sign In">
-              </form>
-              <div class="dropdown-menu-social text text--center">
-                <p>Or sign in with</p>
-                <ul class="list list--inline">
-                  <li><a href="#"><i class="icon icon-google-plus"></i></a></li>
-                  <li><a href="#"><i class="icon icon-facebook"></i></a></li>
-                </ul> 
-              </div>
-              <div class="dropdown-menu-acc">
-                <h3 class="title">Don't have an account?</h3>
-                <input type="submit" class="button button--primary" value="Register">
-              </div>
-            </ul>
-          </li>
+        <ul class="nav nav--main navbar-nav">
+          <li class="active"><a href="#">Top pasiūlymai</a></li>
+          <li><a href="#about">TezTour kelionės</a></li>
+          <li><a href="#about">Novaturo kelionės</a></li>
+          <li><a href="#about">Pažintinės kelionės</a></li>
+          <li><a href="#about">Tolimi kraštai</a></li>
+          <li><a href="#about">Viešbučiai</a></li>
+          <li><a href="#about">Keltų bilietai</a></li>
         </ul>
       </div><!--/.nav-collapse -->
-    </div>
+    </div><!-- .navbar-bottom -->
+
   </div>
 </nav>
 
