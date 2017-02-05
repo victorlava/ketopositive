552ae0b0171a9628d305f52f7d0da6a3bb78ed4c
sveiki@viktoraslava.lt <sveiki@viktoraslava.lt>
2017-02-05 17:21:03 +0000

    added email cta (not rdy yet)

--- partials/email-cta.php
+++ partials/email-cta.php
@@ -1,20 +1,24 @@
 <section class="email section section--primary">  
 	<div class="container">
 		<div class="email-wrapper">
-			<header class="header header--main">
-				<h3 class="header-title">Naujienlaiškio prenumerata</h3>
-				<p class="header-sub-title">Kartą per savaitę siunčiame atrinktus geriausius kelionių pasiū</p>
-			</header>
-			<div class="email-form col-md-6">
-				<form role="form">
-					<div class="input-group email-email">
-		                <input type="email" id="email" name="email" class="form-control" placeholder="Jūsų el. paštas">
-		            </div>
+			<div class="row">
+				<header class="header header--main">
+					<h2 class="header-title">Naujienlaiškio prenumerata</h2> 
+					<p class="header-sub-title">Kartą per savaitę siunčiame atrinktus geriausius kelionių pasiūlymus</p>
+				</header>
+			</div>
+			<div class="row">
+				<div class="email-form">
+					<form role="form">
+						<div class="input-group email-email">
+			                <input type="email" id="email" name="email" class="form-control" placeholder="Jūsų el. paštas">
+			            </div>
 
-		            <div class="input-group email-submit">
-		                <input type="submit" class="button button--primary pull-right" value="Prenumeruoti">
-		            </div>
-				</form>
+			            <div class="input-group email-submit">
+			                <input type="submit" class="button button--primary pull-right" value="Prenumeruoti">
+			            </div>
+					</form>
+				</div>
 			</div>
 		</div>
 	</div>
