bc4195da9bc9761d0f34822cd245fd85688cfc77
sveiki@viktoraslava.lt <sveiki@viktoraslava.lt>
2017-02-13 15:10:00 +0200

    added fililas.php

---
+++ filialas.php
@@ -0,0 +1,66 @@
+
+<?php include('partials/header.php'); ?> 
+
+<body class="page">
+  
+  <?php include('partials/menu.php'); ?>
+
+  <main id="main"> 
+
+  	<div class="container">
+  		<div class="row">
+		  	<header class="header header--top header--main header--line header--center"> 
+				<h2 class="header-title">Vilniaus filialas</h2>
+			</header>
+		</div>
+
+		<div class="row">
+			<div class="col-md-8">
+				<article class="article"> 
+					<p>Paryžiuje bus pristatytas "Gallimard" leidyklos išleistas gidas "Vilnius žemėlapiuose" ("Cartoville Vilnius"). Tai pirmasis ir kol kas vienintelis gidas apie Vilnių prancūzų kalba. Beje, leidinyje trumpai apžvelgiami visi keturi mūsų šalies regionai. Leidykla "Gallimard" gidą "Vilnius žemėlapiuose", be prancūzų, išleido dar anglų ir norvegų kalbomis. Leidėjų teigimu, knygą bus galima įsigyti septynių Europos šalių knygynuose: prancūzų kalba - Prancūzijoje, Belgijoje, Šveicarijoje ir Liuksemburge, anglų - Jungtinėje Karalystėje ir Airijoje, norvegų - Norvegijoje. "Gido apie Vilnių išleidimas - didelis įvykis", - džiaugėsi Lietuvos turizmo informacijos centro Paryžiuje direktorė Inga Lanchas. - Iki šiol Prancūzijoje buvo galima rasti įvairių leidyklų gidų apie Baltijos šalis, dabar prancūzai galės naudotis atskiru gidu, skirtu Lietuvos sostinei. "Pasak direktorės, pastaruoju metu prancūzų susidomėjimas Lietuva, ypač sostine Vilniumi, smarkiai auga. 2008 metais į Vilnių atvyko 21,4 proc. daugiau prancūzų turistų negu</p>
+					<img class="box-shadow" src="/assets/img/src/content-image.jpg" width="903" height="426">
+				</article>
+			</div>
+
+			<div class="sidebar col-md-4"> 
+				<div class="data-block data-block--offer data-block--simple data-block--hoveroff">
+					<h4 class="title">Kontaktai</h4>
+					<div class="work-days">
+						<div class="work-days-list">
+							<ul class="list list--inline">
+								<li class="active"></li>
+								<li class="active"></li>
+								<li class="active"></li>
+								<li class="active"></li>
+								<li class="active"></li>
+								<li class=""></li>
+								<li class=""></li>
+							</ul>
+						</div>
+						<div class="work-days-title">
+							08:00 - 18:00
+						</div>
+					</div>
+					<p> P. Komunos g. 2 (šalia PC Technorama)</br>
+						Tel.: (8-46) 257660</br>
+						Faks.: (8-46) 257660</br>
+						El. paštas: klaipeda@litamicus.lt</br>
+						Darbo laikas: I - V : 9:00 - 18:00
+					</p>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+
+			
+	<div class="gmap">
+		
+	</div>
+		
+
+  </main><!-- #main -->
+
+  	<?php include('partials/email-cta.php'); ?>  
+
+    <?php include('partials/footer.php'); ?>  
