6c14105e54dda7c810fc3ae98800b025341e81d1
sveiki@viktoraslava.lt <sveiki@viktoraslava.lt>
2017-02-20 03:44:06 +0200

    mobile fix

--- assets/js/src/script.js
+++ assets/js/src/script.js
@@ -4,10 +4,20 @@ $(function () {
 
 	$('nav.navbar .navbar-header .navbar-toggle').click(function(){
 		$('#navbar').show();
+		$('html').addClass('no-scroll');
+
+		$('html').on('scroll touchmove mousewheel', function(e){
+			  e.preventDefault();
+			  e.stopPropagation();
+			  return false;
+			})
 	});
 
 	$('#navbar .navbar-close').click(function(){
 		$('#navbar').hide();
+		$('html').removeClass('no-scroll');
+
+	    $('html').off('scroll touchmove mousewheel');
 	});
 
 }); 
