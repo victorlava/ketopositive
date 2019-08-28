88a2daa029b2de1d62f8e4468a10daa568d0f446
Victor <hello@victorlava.com>
2019-08-28 03:54:34 +0300

    email styling

--- partials/email.php
+++ partials/email.php
@@ -1,23 +1,24 @@
 <section class="email">
     <div class="container">
         <div class="email-wrapper">
+            <!-- <i class="fas fa-envelope-open-text"></i> -->
+            <i class="far fa-envelope-open"></i>
+            <!-- <i class="far fa-envelope"></i> -->
             <div class="row">
                 <header class="header header--main">
-                    <h2 class="header-title">Naujienlaiškio prenumerata</h2>
-                    <p class="header-sub-title">Kartą per savaitę siunčiame atrinktus geriausius kelionių pasiūlymus</p>
+                    <h2 class="header-title">Don't Miss New <strong>Keto Recipes</strong> Ever!</h2>
+                    <p class="header-sub-title">Sign up to our weekly mailing list and receive new keto recipes, promotions and expert guidance</p>
                 </header>
             </div>
             <div class="row">
                 <div class="email-form col-md-8">
-
-
                     <form role="form">
                         <div class="email-email col-md-9">
-                            <input type="email" id="email" name="email" class="form-control" placeholder="Jūsų el. paštas">
+                            <input type="email" id="email" name="email" class="form-control" placeholder="Your e-mail address">
                         </div>
 
                         <div class="email-submit col-md-3">
-                            <input type="submit" class="button button--primary pull-right" value="Prenumeruoti">
+                            <input type="submit" class="button button--primary pull-right" value="Subscribe">
                         </div>
                     </form>
                 </div>
